8f1597d74f44b65c2f5f9dee1aecf942d80f0146
caioo0 <cai_oo@sina.com>
2023-04-26 10:50:13 +0800

    引入Sequelize

--- README.md
+++ README.md
@@ -37,5 +37,6 @@ function whenLoadModule(obj) {
 
 ## 教程
 
-- https://juejin.cn/post/7125867746172076069
+- [本项目教程](https://juejin.cn/post/7125867746172076069)
 - [优雅的编写koa代码](https://blog.csdn.net/weixin_36295973/article/details/112311944)
+- [sequelize中文文档](https://www.sequelize.cn/core-concepts/getting-started)
---
+++ database/db.js
@@ -0,0 +1,38 @@
+const Sequelize = require('sequelizze')
+const config = requiere('./config')
+
+console.log('init sequelize...')
+
+//const sequelize = new Sequelize('sqlite::memory:') // Sqlite 示例
+//const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname') // Postgres 示例
+
+const sequelize = new Sequelize(config.database,config.username,config.password,{
+    host:config.hostname,
+    port:config.hostport,
+    dialect:'mysql',    /* 可选 'mysql' | 'mariadb' | 'postgres' | 'mssql'  */
+    pool: {
+        max:5,  //连接池最大连接数量
+        min:0, //最小连接数量
+        idle:10000   // 如果一个线程10s没有被使用过就释放
+    },
+    define:{
+        //schema 和 schemaDelimiter为表前缀，不需要可以删除
+        //schema: 'koa',
+        //连接字符
+        //schemaDelimiter: '-',
+        //是否自动添加时间戳
+        timestamps:false,
+        freezeTableName:true
+    }
+
+})
+
+//测试是否能联通
+
+sequelize.authenticate().then()(() => {
+        console.log("连接成功");
+    }).catch(err => {
+        console.log("连接失败",err);
+})
+
+module.exports = sequelize
